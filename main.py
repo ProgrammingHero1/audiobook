cc147e7011dbb783ff45b537a0e5aa96cb488576
pri-k <64354404+pri-k@users.noreply.github.com>
2020-10-01 00:39:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+
+#opening the pdf file
+name_of_pdf=input("Please enter the name of the file:")
+book = open(name_of_pdf+'.pdf', 'rb')
+
+#reading the text written
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
+#intialising the text to speech
 speaker = pyttsx3.init()
+
+#speaking the text
 for num in range(7, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
