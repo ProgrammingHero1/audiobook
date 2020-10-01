d90337bd6f4b5688c273950f375839c21f54f77f
arsaurav007 <59771339+arsaurav007@users.noreply.github.com>
2020-10-01 11:17:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pyttsx3
 import PyPDF2
+import Translator
 book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
+all=''
 pages = pdfReader.numPages
-
 speaker = pyttsx3.init()
 for num in range(7, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
+    all=text
     speaker.say(text)
     speaker.runAndWait()
+ trans=Translator()
+ t= trans.translate(all,src='en',dest='hi')
+    obj=pyttsx3(text=t.text,slow='False',lang='hi')
+    obj.save("audio.mp3")
