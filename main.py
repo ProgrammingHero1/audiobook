148e202cc27e560c20daf53696198637d738ea4e
ParthaDe94 <72160683+ParthaDe94@users.noreply.github.com>
2020-10-02 10:28:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import PyPDF2
 book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-
+print(pages)
 speaker = pyttsx3.init()
 for num in range(7, pages):
     page = pdfReader.getPage(num)
