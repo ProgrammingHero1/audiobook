1cdaa6bd3be3c85a2a9a57e2be1c4b98cc95d3c9
anshurt <48923412+anshurt@users.noreply.github.com>
2020-10-05 19:08:18 +0530

    Update main.py
    just added some documententation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-import pyttsx3
+# this package is used to convert text to speech
+import pyttsx3 
+#this package is used to read pdf and convert it to text
 import PyPDF2
 book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
+# you can use any number instead of 7 it is just a page number in oop.pdf file
 for num in range(7, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
