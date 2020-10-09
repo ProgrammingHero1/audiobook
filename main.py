db778e5b26cea0fe5fe3761fde87e7928f5d803c
SanchitMisal <72195684+SanchitMisal@users.noreply.github.com>
2020-10-09 23:21:51 +0530

    Audio books new function
    Added a function to start 
    Hope u will like that
    And use that

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import PyPDF2
 book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-
+start =int(input(7))
 speaker = pyttsx3.init()
-for num in range(7, pages):
+for num in range(start, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
