763ef82f75045846b0c53afb7dc8ca1c0975d161
Harmanpreet Singh <59172539+singh-harmanpreet@users.noreply.github.com>
2020-10-14 20:07:27 +0530

    fix: semantic error in loop
    replacing with num from fixed value

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(7, pages):
+for num in range(num, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
