4efc28480d64a9df49a296688c2e001a6ff10c79
Eltaweeel <34998794+Eltaweeel@users.noreply.github.com>
2020-10-26 20:16:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+name = input("Please enter the file name, It must be in the same directory: ")
+book = open(name,'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-
+start = int(input("Please input the starting you want: "))
+# print(pages)
 speaker = pyttsx3.init()
-for num in range(7, pages):
+newVoiceRate = 130
+speaker.setProperty('rate',newVoiceRate)
+for num in range(start, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
