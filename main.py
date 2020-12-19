ab027a70b6966251fb918aadeb0145cc6df37b0c
Preeti Harjani <71420942+preeti1382@users.noreply.github.com>
2020-12-19 10:38:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pyttsx3
 import PyPDF2
+
 book = open('oop.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
-speaker = pyttsx3.init()
-for num in range(7, pages):
-    page = pdfReader.getPage(num)
-    text = page.extractText()
-    speaker.say(text)
+speaker = pyttsx3.init('sapi5')
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[1].id)
+
+n = int(input("Enter the page number from where you want to read:"))
+t = int(input("Enter the page number till you want to read:"))
+if t <= pages:
+    for num in range(n , t):
+        page = pdfReader.getPage(num)
+        text = page.extractText()
+        speaker.say(text)
+        speaker.runAndWait()
+else:
+    speaker.say("Please enter the valid page number..")
     speaker.runAndWait()
