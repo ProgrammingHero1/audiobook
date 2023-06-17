e525d10d6fe444acf617aa4ee6a3259d39e75f8e
Sebastian <68529971+html-proof@users.noreply.github.com>
2023-06-17 20:21:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,41 +4,28 @@ from tkinter import filedialog
 import pyttsx3
 import time
 win = Tk()
-
 win.geometry("1500x1500")
 txt1 = IntVar()
-
 text = Text(win, width=80, height=30)
 text.pack(pady=20)
-
 try:
     def open_pdf():
         file = filedialog.askopenfilename(title="Select a PDF", filetype=(("PDF    Files", "*.pdf"), ("All Files", "*.*")))
         if file:
             pdf_file = PyPDF2.PdfFileReader(file)
-
             page = pdf_file.getPage(txt1.get())
-
             content = page.extractText()
             txt = pyttsx3.init()
             txt.say(content)
             txt.runAndWait()
             text.insert(1.0, content)
             time.sleep(2)
-
-
-
-
 except FloatingPointError:
     pass
-
-
 bt1_open = Button(win, text="Select the file ", command=open_pdf)
 lbl = Label(win, text="input the page to speak")
 lbl2 = Entry(win, textvariable=txt1)
 lbl.pack()
 lbl2.pack()
 bt1_open.pack()
-
-
 win.mainloop()
